1e0c18d7618bf81f773a15e0c76e0947b052cfac
Lee Bankewitz <lee@leebankewitz.com>
2011-01-18 16:20:17 -0500

    better logging when failing to print rerun file

--- lib/testjour/results_formatters/rerun_formatter.rb
+++ lib/testjour/results_formatters/rerun_formatter.rb
@@ -11,6 +11,13 @@ module Testjour
     def result(result)
       if result.failed?
         @failed_scenarios << result.scenario
+
+        if result.scenario.nil?
+          Testjour.logger.debug("FAILED RESULT WITH nil SCENARIO")
+          Testjour.logger.debug("result.status: #{result.status}")
+          Testjour.logger.debug("result.server_id: #{result.server_id}")
+          Testjour.logger.debug("result.message: #{result.message}")
+        end
       end
     end
 
@@ -28,10 +35,6 @@ module Testjour
       File.open("rerun.txt", "w") do |f|
         f.write(failed_features.join(" "))
       end
-    rescue Exception => ex
-      Testjour.logger.error("ERROR PRINTING RERUN FILE: #{ex.message}")
-      Testjour.logger.error(@failed_scenarios.inspect)
-      raise ex
     end
 
   end
